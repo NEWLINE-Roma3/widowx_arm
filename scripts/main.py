9a688b571c29e078f8e51470cd394208b52462ee
andrearlotta <aarlotta@os.uniroma3.it>
2022-05-24 18:13:09 +0200

    modified topics' name on which code is subscribed

--- scripts/main.py
+++ scripts/main.py
@@ -6,9 +6,9 @@ from std_msgs.msg import Float64
 
 def talker():
     
-    pub1 = rospy.Publisher('/widowx_arm/joint_2/command', Float64, queue_size=10)
-    pub2 = rospy.Publisher('/widowx_arm/joint_3/command', Float64, queue_size=10)
-    pub3 = rospy.Publisher('/widowx_arm/joint_4/command', Float64, queue_size=10)
+    pub1 = rospy.Publisher('/widowx_arm/joint_2_controller/command', Float64, queue_size=10)
+    pub2 = rospy.Publisher('/widowx_arm/joint_3_controller/command', Float64, queue_size=10)
+    pub3 = rospy.Publisher('/widowx_arm/joint_4_controller/command', Float64, queue_size=10)
     rospy.init_node('widowx_arm_talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
